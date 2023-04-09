95c9ee463f30a752fd142d4494f034d9a2b783c9
HyoHyun_Home <doch2130@gmail.com>
2023-04-09 20:13:45 +0900

    feat: 2023-04-09 2013 forsection img source add

--- frontend/src/components/MainSection/FourSection.tsx
+++ frontend/src/components/MainSection/FourSection.tsx
@@ -14,8 +14,17 @@ export default function FourSection() {
       </div>
       <div className={style.imgWrap}>
         <img src={HospitalImg1} alt='HospitalImg1' />
+        <div className={style.imgSource}>
+          <a href="https://kr.freepik.com/free-photo/close-up-on-veterinarian-taking-care-of-pet_21080887.htm#query=animal hospital&position=36&from_view=search&track=ais">Freepik</a>
+        </div>
         <img src={HospitalImg2} alt='HospitalImg2' />
+        <div className={style.imgSource}>
+          <a href="https://kr.freepik.com/free-photo/close-up-on-veterinarian-taking-care-of-cat_18395442.htm#query=animal hospital&from_query=동물병원&position=27&from_view=search&track=sph">Freepik</a>
+        </div>
         <img src={HospitalImg3} alt='HospitalImg3' />
+        <div className={style.imgSource}>
+          <a href="https://kr.freepik.com/free-photo/veterinarian-taking-care-of-pet-dog_20823274.htm#page=3&query=animal hospital&position=18&from_view=search&track=ais">Freepik</a>
+        </div>
       </div>
     </div>
   )
