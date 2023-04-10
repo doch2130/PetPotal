90e726ff486d5cb634b90792318a42177dfb1935
DESKTOP-PTLD9MG\ljm92 <ljm920604@gmail.com>
2023-04-10 21:07:26 +0900

    react-router-dom v6 추가  - Error Page 추가  - 기본 react router 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,12 @@
 // import logo from './logo.svg';
 // import './App.css';
 import MainPage from './pages/MainPage';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <MainPage />
+      <Outlet />
     </div>
   );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,10 +2,25 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
+import MainPage from './pages/MainPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {index: true, element: <MainPage />},
+    ]
+  }
+]);
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+  <RouterProvider router={router} />
 );
