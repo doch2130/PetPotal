af12c44a9d8f3d32c7a07b4fba51e2278bd85909
HyoHyun <doch2130@gmail.com>
2023-04-11 14:50:07 +0900

    feat: 2023-04-11 1450 footer complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 // import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
+import Footer from './components/footer/Footer';
 import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <div className="App">
       <MainPage />
+      <Footer />
     </div>
   );
 }
