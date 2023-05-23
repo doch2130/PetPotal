30ebd18ba1443d357717887dd10205312e19ba66
HyoHyun_Home <doch2130@gmail.com>
2023-05-24 00:06:09 +0900

    feat: 2023-05-24 0006

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,7 +1,5 @@
-// import React from 'react';
-
 import MainBeauty from "../components/MainSection/beauty/MainBeauty";
-import MainExplanation from "../components/MainSection/explanation/MainExplanation";
+// import MainExplanation from "../components/MainSection/explanation/MainExplanation";
 import MainFood from "../components/MainSection/food/MainFood";
 import MainHome from "../components/MainSection/home/MainHome";
 import MainHospital from "../components/MainSection/hospital/MainHospital";
@@ -14,7 +12,7 @@ export default function MainPage() {
   return (
     <>
       <MainHome />
-      <MainExplanation />
+      {/* <MainExplanation /> */}
       <MainMate />
       <MainHospital />
       <MainBeauty />
