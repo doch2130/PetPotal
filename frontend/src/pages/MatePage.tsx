78915c76cf098edb29cbdf0bff4bdff3fb548c5e
HyoHyun_Home <doch2130@gmail.com>
2023-06-01 23:39:41 +0900

    Feat: 2023-06-01 MateBoard Working

--- frontend/src/pages/MatePage.tsx
+++ frontend/src/pages/MatePage.tsx
@@ -1,11 +1,12 @@
-// import React from 'react'
-
 import { Link } from "react-router-dom";
+import MateBoard from "../components/Mate/Board/MateBoard";
 
 export default function MatePage() {
   return (
     <div>
       <h1>MatePage</h1>
+      <MateBoard />
+      <br />
       <Link to='/mate/write'>글쓰기</Link>
       <br />
       <Link to='/mate/detail/1'>글보기</Link>
