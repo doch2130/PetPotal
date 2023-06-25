83f7ff617d6eb6b570eac4400e4c87fe12e197c5
HyoHyun_Home <doch2130@gmail.com>
2023-06-25 16:10:42 +0900

    feat: 2023-06-25 1610 mypage chage password

---
+++ frontend/src/components/Mypage/MyInfoPasswordChangeModal.tsx
@@ -0,0 +1,130 @@
+import React, { MouseEventHandler, useEffect } from 'react'
+import style from './MyInfoPasswordChangeModal.module.css';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useAlert } from '../../hooks/useAlert';
+import { useConfirm } from '../../hooks/useConfirm';
+import Controller from '../../api/controller';
+import { useRecoilValue } from 'recoil';
+import { UserType, userState } from '../../recoil/user';
+import { useNavigate } from 'react-router-dom';
+
+interface userChangePasswordFormInput {
+  account: String;
+  currentPassword: String;
+  changePassword: String;
+  changePasswordCheck: String;
+}
+
+interface MyInfoPasswordChangeModalInterface {
+  account: String;
+  onClose: Function;
+  setCertification: Function;
+}
+
+export default function MyInfoPasswordChangeModal(props:MyInfoPasswordChangeModalInterface) {
+  const { account, onClose, setCertification } = props;
+  const { register, setValue, getValues, formState: { errors }, setError, handleSubmit} = useForm<userChangePasswordFormInput>({mode: 'onChange'});
+  const { openAlert } = useAlert();
+  const { openConfirm, closeConfirm } = useConfirm();
+  const controller = new Controller();
+  const navigater = useNavigate();
+
+  useEffect(() => {
+    setValue('account', account);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onSubmit:SubmitHandler<userChangePasswordFormInput> = async (data):Promise<void> => {
+    if(getValues('changePassword') === '') {
+      setError('changePassword', {message: '변경할 비밀번호를 입력해주세요'}, {shouldFocus: true});
+      return ;
+    }
+    if(getValues('changePasswordCheck') === '') {
+      setError('changePasswordCheck', {message: '변경할 비밀번호를 입력해주세요'}, {shouldFocus: true});
+      return ;
+    }
+    if(getValues('changePassword') !== getValues('changePasswordCheck')) {
+      setError('changePasswordCheck', {message: '변경 비밀번호가 일치하지 않습니다'}, {shouldFocus: true});
+      return ;
+    }
+    
+    console.log('data ', data);
+
+    openConfirm({
+      title: '비밀번호 변경 확인',
+      content: '해당 비밀번호로 변경하시겠습니까?',
+      callback: async () => {
+        try {
+          const result = await controller.userChangePassword(data);
+          console.log('result ', result);
+          closeConfirm();
+          onClose();
+          setCertification(false);
+        } catch (err:any) {
+          if (err.response.data.responseCode !== 200) {
+            openAlert({
+              title: '회원정보 비밀번호 변경 에러',
+              type: 'error',
+              content: '비밀번호 변경 중 에러가 발생하였습니다.\r\n새로고침 후 다시 시도해주세요.',
+            });
+            return ;
+          }
+        }
+      }
+    });
+    return ;
+  }
+
+  return (
+    <div className={style.wrap}>
+      <h2>비밀번호 변경</h2>
+      <form className={style.wrapForm} onSubmit={handleSubmit(onSubmit)}>
+        <div className={style.currentPwdWrap}>
+          <label htmlFor='currentPassword'>현재 비밀번호</label>
+          <input 
+            {...register('currentPassword',
+              {
+                required: {value: true, message: '현재 비밀번호를 입력해주세요'},
+              }
+            )}
+            type='password' defaultValue='' placeholder='현재 비밀번호를 입력하세요' id='currentPassword' />
+            <p className={style.warningText}>{errors.currentPassword?.message}</p>
+        </div>
+        <div className={style.changePwdWrap}>
+          <label htmlFor='changePassword'>변경 비밀번호</label>
+          <input 
+            {...register('changePassword',
+              {
+                required: false,
+                pattern: {
+                  value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/i,
+                  message: '대소문자숫자특수기호를 섞어 8자이상을 만들어주세요'
+                },
+              }
+            )}
+            type='password' defaultValue='' placeholder='변경할 비밀번호를 입력하세요' id='changePassword' />
+            <p className={style.warningText}>{errors.changePassword?.message}</p>
+        </div>
+        <div className={style.currentPwdCheckWrap}>
+          <label htmlFor='changePasswordCheck'>변경 비밀번호 확인</label>
+          <input 
+            {...register('changePasswordCheck',
+              {
+                required: false,
+                pattern: {
+                  value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/i,
+                  message: '대소문자숫자특수기호를 섞어 8자이상을 만들어주세요'
+                },
+              }
+            )}
+            type='password' defaultValue='' placeholder='변경할 비밀번호를 다시 입력하세요' id='changePasswordCheck' />
+            <p className={style.warningText}>{errors.changePasswordCheck?.message}</p>
+        </div>
+        <div className={style.buttonWrap}>
+          <button type='button' onClick={onClose as MouseEventHandler}>취소</button>
+          <button type='submit'>수정</button>
+        </div>
+      </form>
+    </div>
+  )
+}
